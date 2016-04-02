1431553e1eb45f6e5c5c2cb252606670689fe7ac
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-04-02 02:36:26 -0400

    client: removed fluff

--- src/client/app/components/RoutePaneComponent.ts
+++ src/client/app/components/RoutePaneComponent.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
 import {RouteRequest} from '../models/RouteRequest'
 import {MaterialPlacesAutocomplete} from './MaterialPlacesAutocomplete';
+import {DropdownComponent} from './DropdownComponent';
 
 @Component({
   selector: 'pane',
-  directives: [MaterialPlacesAutocomplete],
+  directives: [MaterialPlacesAutocomplete, DropdownComponent],
   template: `
     <div id="info-pane">
     <div class="content-wrapper">
@@ -15,9 +16,10 @@ import {MaterialPlacesAutocomplete} from './MaterialPlacesAutocomplete';
            <div class="row">
              <places-input labelText="Start" (placeChanged)="newPlace(value)" ></places-input>
              <places-input labelText="Destination" (placeChanged)="newPlace(value)" ></places-input>
-
+             <dropdown [collection]="carNames" labelText="Car Make" (optionSelected)="selected($event)"></dropdown>
              <div class="input-field col s12">
                <select>
+                 <option selected disabled value="-1">Select an option</option>
                  <option value="1">Vroom</option>
                  <option value="2">Vroom Vroom</option>
                  <option value="3">Vroom Vroom Vroom</option>
@@ -39,6 +41,8 @@ export class Pane {
 
   searchRequest : RouteRequest = new RouteRequest();
 
+  carNames = ["Audi", "Tesla", "Geo Suck", 'Miata', 'Nissan']
+
   constructor() {
 
   }
@@ -46,7 +50,7 @@ export class Pane {
   // ngAfterContentInit is used to initialize the component inside
   // for the fancy selections
   ngAfterContentInit() {
-    // window['$']('select').material_select();
+    window['$']('select').material_select();
   }
 
   beginSearch() {
@@ -54,6 +58,10 @@ export class Pane {
   }
 
   newPlace(value) {
+  }
+
+  selected(index) {
+    console.log('Index for car was selected: ' + index)
   }
 
 }
